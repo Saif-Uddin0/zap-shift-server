97b258b74fda022f2560d5320972d039e2b15531
Saif Uddin <mahirr.846@gmail.com>
2025-12-22 22:19:41 +0600

    added the payment api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const port = process.env.PORT || 3000
 
 // middlware
@@ -32,39 +33,79 @@ async function run() {
     const percelCollection = db.collection('percels')
 
     // percel api
-    app.get('/percels' , async(req,res)=>{
-      const query ={}
-      const {email} = req.query;
-      if(email){
-          query.email = email;
-        }
-        const result = await percelCollection.find(query).sort({bookingDate: -1}).toArray()
-        res.send(result)
+    app.get('/percels', async (req, res) => {
+      const query = {}
+      const { email } = req.query;
+      if (email) {
+        query.email = email;
+      }
+      const result = await percelCollection.find(query).sort({ bookingDate: -1 }).toArray()
+      res.send(result)
+    })
+
+
+    // single percel id for payment
+    app.get('/percels/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await percelCollection.findOne(query)
+      res.send(result)
     })
 
     // post
-    app.post('/percels' , async(req,res)=>{
-        const percel = req.body;
-        const result = await percelCollection.insertOne(percel)
-        res.send(result)
+    app.post('/percels', async (req, res) => {
+      const percel = req.body;
+      const result = await percelCollection.insertOne(percel)
+      res.send(result)
     })
 
     // delete
-    app.delete('/percels/:id', async(req , res)=>{
+    app.delete('/percels/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await percelCollection.deleteOne(query);
       res.send(result)
     })
 
 
+    // payment related apis
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: 'USD',
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName
+              },
+            },
+
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.email,
+        mode: 'payment',
+        metadata: {
+          parcelId: paymentInfo.parcelId
+        },
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      })
+      console.log(session);
+      res.send({ url: session.url })
+
+    })
+
 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
+  }
   finally {
-    
+
   }
 }
 
